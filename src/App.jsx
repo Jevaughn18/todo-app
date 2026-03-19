d04a1ff3e4bceb3ff110a92fc444dfe1d8f1c73b
Jevaughn Stewart <148567685+Jevaughn18@users.noreply.github.com>
2026-03-18 22:23:08 -0500

    Initial react-app project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>todo-app</h1>
+        <p>Welcome to your new React application!</p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
